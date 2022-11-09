277ac694d7e5719ebd33938dd7bcb0415901b216
marketaanezka <marketawillis1@gmail.com>
2022-11-09 13:55:18 +0100

    add images

--- src/components/FeaturedPitch/Carousel.tsx
+++ src/components/FeaturedPitch/Carousel.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import juniorguru from "../../../public/img/juniorguru.png";
 import wdf from "../../../public/img/wdf.png";
 import jury from "../../../public/img/jurapodcast.jpeg";
+import heroine from "../../../public/img/heroine.png";
 import styles from "./Carousel.module.scss";
 
 const Carousel = () => {
@@ -17,6 +18,12 @@ const Carousel = () => {
       </div>
       <Image src={wdf} alt="wdf logo" width={100} height={100} />
       <Image src={jury} alt="svet podle jury logo" width={100} height={100} />
+      <Image
+        src={heroine}
+        alt="magazine heroine logo"
+        width={100}
+        height={100}
+      />
       <button onClick={() => console.log("hi button")}>Click me d</button>
     </>
   );
