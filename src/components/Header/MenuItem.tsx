c0bb4e0c2151745fbfc853ac1c05d4b220e363d3
marketaanezka <marketawillis1@gmail.com>
2023-08-01 00:49:43 +0200

    implement page with chakra form

--- src/components/Header/MenuItem.tsx
+++ src/components/Header/MenuItem.tsx
@@ -5,23 +5,16 @@ import styles from "./MenuItem.module.scss";
 interface MenuItemProps {
   href: string;
   isActive: boolean;
-  key: string;
   children: ReactNode;
   onClick?: () => void;
 }
 
-const MenuItem: FC<MenuItemProps> = ({
-  href,
-  isActive,
-  key,
-  children,
-  onClick,
-}) => {
+const MenuItem: FC<MenuItemProps> = ({ href, isActive, children, onClick }) => {
   return (
     <Link
       href={href}
       className={`${styles.link} ${isActive ? styles.isActive : ""}`}
-      key={key}
+      key={href}
       onClick={onClick}
     >
       {children}
