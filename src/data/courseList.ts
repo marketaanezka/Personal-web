5309e990d4ecaa224a37c195936f5209c7d0a13d
marketaanezka <marketawillis1@gmail.com>
2025-01-27 12:08:53 +0100

    add recording info to course

--- src/data/courseList.ts
+++ src/data/courseList.ts
@@ -5,7 +5,7 @@ export const courseList: CourseCard[] = [
     link: "https://willis.webinarninja.com/live-webinars/10639265/register?in_tok=e838b3d5-8fd7-4b75-940e-de3c8d9558f4",
     image:
       "https://res.cloudinary.com/doqtwlcxw/image/upload/w_800,f_auto,q_auto/9663b44c-f090-4365-b33f-2be4b2489ddb_fqw6h4.jpg",
-    time: "9:00-12:00",
+    time: "9:00-12:00 & záznam 14 dní",
     date: "8. 2. 2025",
     price: "990,-",
     alt: "Kurz Jak si najít práci v IT v roce 2025",
